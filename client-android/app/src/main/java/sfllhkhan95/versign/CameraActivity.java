4a8dec76e5a1f6af226283520787d5671f184a3c
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2017-04-25 03:12:25 +0500

    Created Android project for client application.

---
+++ client-android/app/src/main/java/sfllhkhan95/versign/CameraActivity.java
@@ -0,0 +1,13 @@
+package sfllhkhan95.versign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
