73e5affc8dc07e67425c75d4ead62ccc8e3cb873
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2017-04-25 10:02:02 +0500

    Added Person model class.

---
+++ client-android/app/src/main/java/sfllhkhan95/versign/model/Person.java
@@ -0,0 +1,36 @@
+package sfllhkhan95.versign.model;
+
+/**
+ * @author saifkhichi96
+ */
+public class Person {
+
+    private String CNIC;
+    private String givenName;
+    private String familyName;
+
+    public String getCNIC() {
+        return CNIC;
+    }
+
+    public void setCNIC(String CNIC) {
+        this.CNIC = CNIC;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+}
