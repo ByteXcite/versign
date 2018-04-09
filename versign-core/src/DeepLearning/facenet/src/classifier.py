6d72b8c57c14a28f9e58828bb936f5b5cbe13eb4
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2018-04-09 09:47:43 +0500

    Changed facenet source to our requirements

--- versign-core/src/DeepLearning/facenet/src/classifier.py
+++ versign-core/src/DeepLearning/facenet/src/classifier.py
@@ -1,19 +1,19 @@
 """An example of how to use your own dataset to train a classifier that recognizes people.
 """
 # MIT License
-#
+# 
 # Copyright (c) 2016 David Sandberg
-#
+# 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-#
+# 
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-#
+# 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,48 +33,48 @@ import facenet
 import os
 import sys
 import math
+import scipy.io
 import pickle
 from sklearn.svm import SVC
 
 def main(args):
-
+  
     with tf.Graph().as_default():
-
+      
         with tf.Session() as sess:
-
+            
             np.random.seed(seed=args.seed)
-
+            
             if args.use_split_dataset:
                 dataset_tmp = facenet.get_dataset(args.data_dir)
                 train_set, test_set = split_dataset(dataset_tmp, args.min_nrof_images_per_class, args.nrof_train_images_per_class)
-                if (args.mode=='TRAIN'):
-                    dataset = train_set
-                elif (args.mode=='CLASSIFY'):
-                    dataset = test_set
+                #if (args.mode=='TRAIN'):
+                dataset = train_set
+                #elif (args.mode=='CLASSIFY'):
+                #    dataset = test_set
             else:
                 dataset = facenet.get_dataset(args.data_dir)
 
             # Check that there are at least one training image per class
             for cls in dataset:
-                assert(len(cls.image_paths)>0, 'There must be at least one image for each class in the dataset')
+                assert(len(cls.image_paths)>0, 'There must be at least one image for each class in the dataset')            
 
-
+                 
             paths, labels = facenet.get_image_paths_and_labels(dataset)
-
+            
             print('Number of classes: %d' % len(dataset))
             print('Number of images: %d' % len(paths))
-
+            
             # Load the model
             print('Loading feature extraction model')
             facenet.load_model(args.model)
-
+            
             # Get input and output tensors
             images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
             embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
             phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
             embedding_size = embeddings.get_shape()[1]
-            print(embedding_size)
-
+            
             # Run forward pass to calculate embeddings
             print('Calculating features for images')
             nrof_images = len(paths)
@@ -87,61 +87,18 @@ def main(args):
                 images = facenet.load_data(paths_batch, False, False, args.image_size)
                 feed_dict = { images_placeholder:images, phase_train_placeholder:False }
                 emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
-
-            classifier_filename_exp = os.path.expanduser(args.classifier_filename)
-
-            if (args.mode=='TRAIN'):
-                # Train classifier
-                print('Training classifier')
-                model = SVC(kernel='linear', probability=True)
-                model.fit(emb_array, labels)
-
-                # Create a list of class names
-                class_names = [ cls.name.replace('_', ' ') for cls in dataset]
-
-                # Saving classifier model
-                with open(classifier_filename_exp, 'wb') as outfile:
-                    pickle.dump((model, class_names), outfile)
-                print('Saved classifier model to file "%s"' % classifier_filename_exp)
-
-            elif (args.mode=='CLASSIFY'):
-                # Classify images
-                print('Testing classifier')
-                with open(classifier_filename_exp, 'rb') as infile:
-                    (model, class_names) = pickle.load(infile)
-
-                print('Loaded classifier model from file "%s"' % classifier_filename_exp)
-
-                predictions = model.predict_proba(emb_array)
-                print(predictions.shape)
-                # best_class_indices = np.argmax(predictions, axis=1)
-
-                # best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
-
-
-                # for i in range(len(best_class_indices)):
-                #     print('%4d  %s: %.3f' % (i, class_names[best_class_indices[i]], best_class_probabilities[i]))
-
-                predictions_for_my_class = predictions[:,0]
-                print(predictions_for_my_class.shape)
-
-                # variying thresholds from (0.1 to 0.9)
-                for j in np.arange(0.0, 1.0, 0.1):
-                    genuines = 0
-                    for i in range(predictions_for_my_class.shape[0]):
-                        x = predictions_for_my_class[i]>j
-                        if x:
-                            genuines = genuines+1
-                        print('%4d  %.4f %r'%(i,predictions_for_my_class[i],x))
-                    print(genuines)
-                    print('---'*10)
-
-                # print(np.max(predictions_for_my_class))
-                # print(np.min(predictions_for_my_class))
-                # accuracy = np.mean(np.equal(best_class_indices, labels))
-                # print('Accuracy: %.3f' % accuracy)
-
-
+            
+            save_path = args.data_dir
+            print("Saving extracted features in", save_path)
+            print(emb_array)
+            i=0
+            for r in emb_array:
+                save_filename = os.path.join(save_path, 'R' + str(i) + '.mat')
+                feature_vector = np.array(r)
+                scipy.io.savemat(save_filename, {'feature_vector':feature_vector})
+                i+=1
+                
+            
 def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_class):
     train_set = []
     test_set = []
@@ -154,22 +111,22 @@ def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_clas
             test_set.append(facenet.ImageClass(cls.name, paths[nrof_train_images_per_class:]))
     return train_set, test_set
 
-
+            
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('mode', type=str, choices=['TRAIN', 'CLASSIFY'],
-        help='Indicates if a new classifier should be trained or a classification ' +
-        'model should be used for classification', default='CLASSIFY')
+    
+    #parser.add_argument('mode', type=str, choices=['TRAIN', 'CLASSIFY'],
+    #   help='Indicates if a new classifier should be trained or a classification ' + 
+    #    'model should be used for classification', default='CLASSIFY')
     parser.add_argument('data_dir', type=str,
         help='Path to the data directory containing aligned LFW face patches.')
-    parser.add_argument('model', type=str,
+    parser.add_argument('model', type=str, 
         help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
-    parser.add_argument('classifier_filename',
-        help='Classifier model file name as a pickle (.pkl) file. ' +
-        'For training this is the output and for classification this is an input.')
-    parser.add_argument('--use_split_dataset',
-        help='Indicates that the dataset specified by data_dir should be split into a training and test set. ' +
+    #parser.add_argument('classifier_filename', 
+    #    help='Classifier model file name as a pickle (.pkl) file. ' + 
+    #    'For training this is the output and for classification this is an input.')
+    parser.add_argument('--use_split_dataset', 
+        help='Indicates that the dataset specified by data_dir should be split into a training and test set. ' +  
         'Otherwise a separate test set can be specified using the test_data_dir option.', action='store_true')
     parser.add_argument('--test_data_dir', type=str,
         help='Path to the test data directory containing aligned images used for testing.')
@@ -183,7 +140,7 @@ def parse_arguments(argv):
         help='Only include classes with at least this number of images in the dataset', default=20)
     parser.add_argument('--nrof_train_images_per_class', type=int,
         help='Use this number of images from each class for training and the rest for testing', default=10)
-
+    
     return parser.parse_args(argv)
 
 if __name__ == '__main__':
