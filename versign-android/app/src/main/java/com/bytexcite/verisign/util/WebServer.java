e6a3e64e8ac5738080fd07969b92d3054dbeca7a
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2018-04-16 01:56:16 +0500

    Updated server address

--- versign-android/app/src/main/java/com/bytexcite/verisign/util/WebServer.java
+++ versign-android/app/src/main/java/com/bytexcite/verisign/util/WebServer.java
@@ -6,8 +6,8 @@ import sfllhkhan95.android.rest.HttpServer;
 
 
 public class WebServer extends HttpServer {
-    // private static final String SERVER_ADDRESS = "https://versign.azurewebsites.net/app/";
-    private static final String SERVER_ADDRESS = "http://192.168.43.223/projects/apps/VerSign/server/";
+    private static final String SERVER_ADDRESS = "http://verisign.saifkhichi.com/src/controller/";
+    // private static final String SERVER_ADDRESS = "http://192.168.43.223/projects/apps/VerSign/versign-web/src/controller/";
 
     public WebServer() throws MalformedURLException {
         super(SERVER_ADDRESS);
